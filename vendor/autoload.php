fba525328b937f2cfcba1a4f78b22bf00d11e262
Herb Miller <herb@bobbingwide.com>
2022-08-16 10:14:37 +0100

    woocommerce 6.8.0 - 2022-08-09

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -9,4 +9,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit8e39beabf0b904516e54eae4ce6f8d7b::getLoader();
+return ComposerAutoloaderInit2209626d89ea551641d9127876ba3fec::getLoader();
